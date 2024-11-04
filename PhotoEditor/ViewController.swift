256513c518a8403f6d9e5ed2fcf9a48548d8c6bc
Daniil Davidovich <113355546+DaniilDavidovich@users.noreply.github.com>
2024-11-04 11:05:57 +0300

    Initial Commit

---
+++ PhotoEditor/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PhotoEditor
+//
+//  Created by Daniil Davidovich on 4.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+    }
+
+}
+
