aef598d8f5bbc8a196dfa385decb3e2a469d4d7f
Daniil Davidovich <113355546+DaniilDavidovich@users.noreply.github.com>
2024-11-11 13:54:26 +0300

    setup UIMenu, fix pinch zoom

--- PhotoEditor/Sources/Flow/PhotoEditorViewControoler/PhotoEditorViewController.swift
+++ PhotoEditor/Sources/Flow/PhotoEditorViewControoler/PhotoEditorViewController.swift
@@ -12,7 +12,7 @@ final class PhotoEditorViewController: UIViewController {
     //MARK: - Properties
     
     private var viewModel: PhotoEditorViewModel
-
+    
     //MARK: - UIElements
     
     private lazy var plusButton: UIButton = {
@@ -29,7 +29,6 @@ final class PhotoEditorViewController: UIViewController {
         view.layer.borderWidth = 2
         view.layer.borderColor = UIColor.systemBlue.cgColor
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.isHidden = true
         return view
     }()
     
@@ -37,16 +36,7 @@ final class PhotoEditorViewController: UIViewController {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         imageView.isUserInteractionEnabled = true
-        imageView.isHidden = true
         return imageView
-    }()
-    
-    private lazy var filterSwitch: UISwitch = {
-        let toggle = UISwitch()
-        toggle.addTarget(self, action: #selector(toggleSwitch), for: .valueChanged)
-        toggle.translatesAutoresizingMaskIntoConstraints = false
-        toggle.isHidden = true
-        return toggle
     }()
     
     
@@ -68,33 +58,20 @@ final class PhotoEditorViewController: UIViewController {
         setupHierarchy()
         setupLayout()
         setupGestures()
+        needShowEditor(isVisible: false)
     }
     
     //MARK: - Setups
     
     private func setupView() {
-        self.title = "Editor"
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithDefaultBackground()
-        navigationController?.navigationBar.standardAppearance = appearance
-        navigationController?.navigationBar.scrollEdgeAppearance = appearance
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save",
-                                                            style: .done,
-                                                            target: self,
-                                                            action: #selector(saveImage))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Reset",
-                                                           style: .plain,
-                                                           target: self,
-                                                           action: #selector(resetDidTapped))
-        navigationItem.leftBarButtonItem?.isHidden = true
-        navigationItem.rightBarButtonItem?.isHidden = true
+        self.view.backgroundColor = .systemGray6
+        setupNavigationBar()
     }
     
     private func setupHierarchy() {
         view.addSubview(plusButton)
         view.addSubview(frameView)
         frameView.addSubview(imageView)
-        view.addSubview(filterSwitch)
     }
     
     private func setupLayout() {
@@ -113,9 +90,6 @@ final class PhotoEditorViewController: UIViewController {
             imageView.trailingAnchor.constraint(equalTo: frameView.trailingAnchor),
             imageView.topAnchor.constraint(equalTo: frameView.topAnchor),
             imageView.bottomAnchor.constraint(equalTo: frameView.bottomAnchor),
-            
-            filterSwitch.topAnchor.constraint(equalTo: frameView.bottomAnchor, constant: 20),
-            filterSwitch.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
     }
     
@@ -126,10 +100,47 @@ final class PhotoEditorViewController: UIViewController {
         pinchGesture.delegate = self
         let rotateGesture = UIRotationGestureRecognizer(target: self, action: #selector(handleRotate))
         rotateGesture.delegate = self
-        imageView.addGestureRecognizer(panGesture)
+        frameView.addGestureRecognizer(panGesture)
         imageView.addGestureRecognizer(pinchGesture)
         imageView.addGestureRecognizer(rotateGesture)
     }
+    
+    private func setupNavigationBar() {
+        self.title = "Editor"
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        let saveButton = UIBarButtonItem(title: "Save",
+                                         style: .done,
+                                         target: self,
+                                         action: #selector(saveImage))
+        
+        let filterButton = UIBarButtonItem(title: nil,
+                                           image: UIImage(systemName: "camera.filters"),
+                                           primaryAction: nil,
+                                           menu: self.setupUIMenu())
+   
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Reset",
+                                                           style: .plain,
+                                                           target: self,
+                                                           action: #selector(resetDidTapped))
+        navigationItem.rightBarButtonItems = [saveButton, filterButton]
+    }
+    
+    
+    private func setupUIMenu() -> UIMenu  {
+        return UIMenu(title: "", children: [
+            UIAction(title: "Turn on", image: nil, handler: { [weak self] _ in
+                self?.viewModel.applyFilter(isOn: true)
+            }),
+            UIAction(title: "Turn off", image: nil, handler: { [weak self] _ in
+                self?.viewModel.applyFilter(isOn: false)
+            }),
+        ])
+    }
+    
     
     //MARK: - Methods
     
@@ -145,28 +156,37 @@ final class PhotoEditorViewController: UIViewController {
     @objc private func handlePan(gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: frameView)
         if let view = gesture.view {
-            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+            imageView.center = CGPoint(x: imageView.center.x + translation.x, y: imageView.center.y + translation.y)
             gesture.setTranslation(.zero, in: frameView)
         }
     }
     
     // Handle pinch gesture for zooming in/out the image
     @objc private func handlePinch(gesture: UIPinchGestureRecognizer) {
-        if let view = gesture.view {
-            view.transform = view.transform.scaledBy(x: gesture.scale, y: gesture.scale)
-            gesture.scale = 1.0
-        }
+        guard let gestureView = gesture.view else { return }
+        
+        // Get the location of the pinch gesture within the imageView
+        let pinchLocation = gesture.location(in: imageView)
+        
+        // Calculate the new scale
+        let scale = gesture.scale
+        
+        // Adjust the transform of imageView, scaling around the pinch location
+        gestureView.transform = gestureView.transform
+            .translatedBy(x: pinchLocation.x - gestureView.bounds.midX, y: pinchLocation.y - gestureView.bounds.midY)
+            .scaledBy(x: scale, y: scale)
+            .translatedBy(x: gestureView.bounds.midX - pinchLocation.x, y: gestureView.bounds.midY - pinchLocation.y)
+        
+        // Reset the scale factor to 1 to incrementally adjust the scaling
+        gesture.scale = 1.0
     }
+    
     
     @objc private func handleRotate(gesture: UIRotationGestureRecognizer) {
         if let view = gesture.view {
-            view.transform = view.transform.rotated(by: gesture.rotation)
+            imageView.transform = imageView.transform.rotated(by: gesture.rotation)
             gesture.rotation = 0
         }
-    }
-    
-    @objc private func toggleSwitch() {
-        viewModel.applyFilter(isOn: filterSwitch.isOn)
     }
     
     @objc private func saveImage() {
@@ -177,6 +197,11 @@ final class PhotoEditorViewController: UIViewController {
                 AllertHelper.presentErrorAllert(in: self)
             }
         }
+    }
+    
+    @objc private func applyFilter() {
+        
+        viewModel.applyFilter(isOn: true)
     }
     
     @objc private func resetDidTapped() {
@@ -203,19 +228,16 @@ extension PhotoEditorViewController: PhotoEditorViewModelDelegate2 {
     func needUpdateImage(with image: UIImage?) {
         imageView.image = image
         imageView.frame = frameView.bounds
+        imageView.transform = .init(scaleX: 0, y: 0)
+        imageView.transform  = .init(rotationAngle: 0)
     }
     
     func needShowEditor(isVisible: Bool) {
         plusButton.isHidden = isVisible
         imageView.isHidden = !isVisible
         frameView.isHidden = !isVisible
-        filterSwitch.isHidden = !isVisible
-        navigationItem.rightBarButtonItem?.isHidden = !isVisible
+        navigationItem.rightBarButtonItems?.forEach { $0.isHidden = !isVisible }
         navigationItem.leftBarButtonItem?.isHidden = !isVisible
-        
-        if isVisible {
-            filterSwitch.isOn = false
-        }
     }
     
     func didUpdateImageAfterApplyFilter(with image: UIImage) {
